f289e4b5ce4c92dedea9bc6a5ae115bda09a407a
frankot <frankiantki@gmail.com>
2026-03-10 12:06:39 +0100

    nrf

--- src/app/(customerFacing)/layout.tsx
+++ src/app/(customerFacing)/layout.tsx
@@ -12,8 +12,10 @@ export default async function CustomerFacingLayout({ children }: { children: Rea
     return (
       <div className="fixed inset-0 z-[9999] flex h-screen w-screen overflow-hidden bg-white text-black">
         <div className="mx-auto flex w-full max-w-5xl flex-col items-center justify-center px-6 text-center">
-          <p className="text-4xl font-extrabold tracking-[0.18em]  sm:text-8xl">mami</p>
-          <p className="mt-10 text-xl font-medium tracking-[0.3em] uppercase sm:text-2xl">Coming Soon</p>
+          <p className="text-4xl font-extrabold tracking-[0.18em] sm:text-8xl">mami</p>
+          <p className="mt-10 text-xl font-medium tracking-[0.3em] uppercase sm:text-2xl">
+            Coming Soon
+          </p>
         </div>
       </div>
     )
